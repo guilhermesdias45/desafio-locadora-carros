ef8e8249f82dc7d934f4551f57a6b13135e88977
guilhermesdias45 <guilherme.sdias@sptech.school>
2026-05-12 15:46:07 -0300

    feat: Construção do WebClientConfig

--- aluguel/src/main/java/school/sptech/aluguel/service/AluguelService.java
+++ aluguel/src/main/java/school/sptech/aluguel/service/AluguelService.java
@@ -1,6 +1,7 @@
 package school.sptech.aluguel.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import school.sptech.aluguel.dto.AluguelRequestDTO;
 import school.sptech.aluguel.exception.NotFoundException;
 import school.sptech.aluguel.mapper.AluguelMapper;
@@ -13,8 +14,14 @@ import java.math.BigDecimal;
 public class AluguelService {
     private final AluguelRepository repository;
 
-    public AluguelService(AluguelRepository repository) {
+    public WebClient motoristaWebClient;
+
+    public WebClient carroWebClient;
+
+    public AluguelService(AluguelRepository repository, WebClient.Builder webClientBuilder) {
         this.repository = repository;
+        this.motoristaWebClient = webClientBuilder.baseUrl("http://localhost:8080").build();
+        this.carroWebClient = webClientBuilder.baseUrl("http://localhost:8082").build();
     }
 
     public Aluguel salvar(AluguelRequestDTO dto){
