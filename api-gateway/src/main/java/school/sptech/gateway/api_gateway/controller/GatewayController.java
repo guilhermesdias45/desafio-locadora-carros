fa544e8315dd3347539d1a9cefaa433b6a4722ec
guilhermesdias45 <guilherme.sdias@sptech.school>
2026-05-13 14:19:37 -0300

    feat: Aplicando e configurando o token JWT

---
+++ api-gateway/src/main/java/school/sptech/gateway/api_gateway/controller/GatewayController.java
@@ -0,0 +1,52 @@
+package school.sptech.gateway.api_gateway.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api")
+public class GatewayController {
+    private final WebClient webClient;
+
+    public GatewayController() {
+        this.webClient = WebClient.builder().build();
+    }
+
+    @RequestMapping("/{service}/**")
+    public Mono<ResponseEntity<String>> proxy(
+            @PathVariable String service,
+            @RequestHeader HttpHeaders headers,
+            @RequestParam(required = false)MultiValueMap<String, String> queryParams,
+            @RequestBody(required = false) Mono<String> body,
+            ServerHttpRequest request
+            ){
+
+        String baseUrl = switch (service){
+            case "carros" -> "";
+            case "alugueis" -> "";
+            case "motoristas" -> "";
+            default -> null;
+        };
+
+        if (baseUrl == null){
+            return Mono.just(ResponseEntity.status(400).body("Serviço " + service + " não encontrado."));
+        }
+
+        String fullPath = request.getURI().getRawPath().replace("/api/" + service, "");
+
+        String urlCompleta = (request.getURI().getRawQuery() != null) ?
+                baseUrl + fullPath + "?" + request.getURI().getRawQuery() : baseUrl + fullPath;
+
+        return webClient.method(request.getMethod())
+                .uri(urlCompleta)
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .body(body == null ? Mono.empty() : body, String.class)
+                .retrieve()
+                .toEntity(String.class);
+    }
+}
